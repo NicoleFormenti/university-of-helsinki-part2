a16f6f577947efc44bb8cb4eac961d3726caf217
Nicky <133660535+NicoleFormenti@users.noreply.github.com>
2023-08-24 17:33:26 +0100

    Create SearchBar.jsx

---
+++ part2.6-2.10/src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+    return (
+        <div>
+            <h3>Search a person:</h3>
+            <input type="text" placeholder="search name..." />
+        </div>
+    )
+}
+export default SearchBar;
