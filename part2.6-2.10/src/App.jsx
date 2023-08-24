a1aa1b41683d4f758583f74c12bbc684b23ef9be
Nicky <133660535+NicoleFormenti@users.noreply.github.com>
2023-08-24 16:21:34 +0100

    Create App.jsx

---
+++ part2.6-2.10/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import DisplayEntries from "./components/DisplayEntries";
+import EntryForm from "./components/EntryForm";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+
+  const addEntryToPhoneBook = (entry) => {
+    setPersons(
+      [...persons, entry].sort((a, b) =>
+        a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+      )
+    );
+  };
+
+  return (
+    <div>
+    <h2>My Phone Book</h2>
+    <h3>Add a person</h3>
+    <div style={{display: "flex", flexDirection: "column", width: '300px'}}>
+      <EntryForm addEntryToPhoneBook={addEntryToPhoneBook} />
+      <h3>Persons added:</h3>
+      <DisplayEntries persons={persons} />
+    </div>
+    </div>
+  );
+}
+
+export default App;
